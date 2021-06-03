bccb7185f95b5cfe0c69033fc23b53be1ae35e4a
Clara Cook <claracook@LPS-168890.local>
2021-06-03 11:06:47 -0400

    04 done

---
+++ 04_jump_the_five/jump.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Jump the Five"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Jump the Five',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text', metavar='str', help='Input text')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    jumper = {'1': '9', '2': '8', '3': '7', '4': '6', '5': '0',
+              '6': '4', '7': '3', '8': '2', '9': '1', '0': '5'}
+
+    message = ''
+    for char in args.text:
+        if char in jumper.keys():
+            message += jumper[char]
+        else:
+            message += char
+    print(message)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
